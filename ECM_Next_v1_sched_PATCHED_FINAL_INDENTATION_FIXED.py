cd1e1b86417a7177e41388bb792879d824f60cf3
jfkarle <jfkarle@gmail.com>
2025-05-19 10:09:57 -0400

    Update ECM_Next_v1_sched_PATCHED_FINAL_INDENTATION_FIXED.py

--- ECM_Next_v1_sched_PATCHED_FINAL_INDENTATION_FIXED.py
+++ ECM_Next_v1_sched_PATCHED_FINAL_INDENTATION_FIXED.py
@@ -85,7 +85,7 @@ def generate_slots_for_high_tide(high_tide_ts: str):
 def get_valid_slots_with_tides(date: datetime, ramp: str):
     preds, err = get_tide_predictions(date, ramp)
     if err or not preds:
-        return [], [], []
+        return [], []
     st.write("First few 'preds':", preds[:2])  # Debugging line
     high_tides_data = [(datetime.strptime(p['t'], "%Y-%m-%d %H:%M"), p['type']) for p in preds if p['type'] == 'H']
     slots = []
@@ -94,7 +94,7 @@ def get_valid_slots_with_tides(date: datetime, ramp: str):
         high_tide_times.append(ht_datetime.strftime("%I:%M %p"))
         ht_ts = ht_datetime.strftime("%Y-%m-%d %H:%M")
         slots.extend(generate_slots_for_high_tide(ht_ts))
-    return sorted(set(slots)), high_tide_times, high_tides_data
+    return sorted(set(slots)), high_tide_times
 
 
 def is_workday(date: datetime):
