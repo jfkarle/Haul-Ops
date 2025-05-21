ed2087c00704a50bb0eaa928c002f11ef1ef1bbf
jfkarle <jfkarle@gmail.com>
2025-05-21 12:55:38 -0400

    Update Streamlit_app_FIXED_INDENTATION.py

--- Streamlit_app_FIXED_INDENTATION.py
+++ Streamlit_app_FIXED_INDENTATION.py
@@ -402,7 +402,7 @@ if st.session_state["schedule"]:
 
     schedule_df_display = pd.DataFrame(display_schedule_list)
     st.dataframe(schedule_df_display[[
-        "Customer", "Boat Name", "Boat Type", "Date", "Time", "Truck", "Truck J17", "Duration (hrs)"
+        "Customer", "Boat Type", "Date", "Time", "Truck", "Truck J17", "Duration (hrs)"
     ]])
 else:
     st.info("The schedule is currently empty.")
