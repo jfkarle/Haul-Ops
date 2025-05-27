5079385af077f6e7d7840a0a42be2477f40334fd
jfkarle <jfkarle@gmail.com>
2025-05-27 16:11:38 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -790,7 +790,8 @@ with st.sidebar:
                         try:
                             tide_time_dt = datetime.strptime(item['time'], "%Y-%m-%d %H:%M")
                             if time(5, 0) <= tide_time_dt.time() <= time(19, 0):
-                                formatted_time = format_time(item['time'].split()[-1])
+                                tide_dt = datetime.strptime(item['time'], "%Y-%m-%d %H:%M")
+                            formatted_time = tide_dt.strftime("%I:%M %p")
                                 filtered_tides_display.append(f"- {formatted_time} ({item['type']})")
                         except ValueError as e:
                             print(f"Error parsing time: {e}")
