6f6fa719f75fbf5f8c56fb09c4990c9d0d8a7de8
jfkarle <jfkarle@gmail.com>
2025-05-28 19:06:25 -0400

    Update Streamlit_App2_CLEANED.py

--- Streamlit_App2_CLEANED.py
+++ Streamlit_App2_CLEANED.py
@@ -738,31 +738,31 @@ if selected_customer:
     noaa_station_id = RAMP_TO_NOAA_ID.get(ramp_choice) or "8445138"
     tide_data_result = get_tide_predictions(earliest_date_input, ramp_choice)
     if len(tide_data_result) == 2:
-        tide_predictions, err = tide_data_result
-        if tide_predictions:
-            filtered_tides_display = []
-            for item in tide_predictions:
-                try:
-                    tide_time_dt = datetime.strptime(item['time'], "%Y-%m-%d %H:%M")
-                    if time(5, 0) <= tide_time_dt.time() <= time(19, 0):
-                        formatted_time = format_time(item['time'].split()[-1])
-                        filtered_tides_display.append(f"- {formatted_time} ({item['type']})")
-                except ValueError as e:
-                    print(f"Error parsing time: {e}")
-
-            if filtered_tides_display:
-                tide_display_text = "Tides (5 AM - 7 PM):\n" + "\n".join(filtered_tides_display)
-                st.sidebar.info(tide_display_text)
-            else:
-                st.sidebar.info("No high or low tide data available between 5 AM and 7 PM for this date and ramp.")
-        elif err:
-            st.sidebar.warning(f"Could not retrieve tide information. Error: {err}")
+    tide_predictions, err = tide_data_result
+    if tide_predictions:
+        filtered_tides_display = []
+        for item in tide_predictions:
+            try:
+                tide_time_dt = datetime.strptime(item['time'], "%Y-%m-%d %H:%M")
+                if time(5, 0) <= tide_time_dt.time() <= time(19, 0):
+                    formatted_time = format_time(item['time'].split()[-1])
+                    filtered_tides_display.append(f"- {formatted_time} ({item['type']})")
+            except ValueError as e:
+                print(f"Error parsing time: {e}")
+        if filtered_tides_display:
+            tide_display_text = "Tides (5 AM - 7 PM):\n" + "\n".join(filtered_tides_display)
+            st.sidebar.info(tide_display_text)
         else:
-            st.sidebar.info("No tide data available.")
-    elif len(tide_data_result) == 1:
-        st.sidebar.warning("Tide data returned in unexpected format.")
+            st.sidebar.info("No high or low tide data available between 5 AM and 7 PM for this date and ramp.")
+    elif err:
+        st.sidebar.warning(f"Could not retrieve tide information. Error: {err}")
     else:
-        st.sidebar.warning("Unable to retrieve tide predictions.")
+        st.sidebar.info("No tide data available.")
+elif len(tide_data_result) == 1:
+    st.sidebar.warning("Tide data returned in unexpected format.")
+else:
+    st.sidebar.warning("Unable to retrieve tide predictions.")
+
 
     
         #  -----  END HIGH/LOW TIDE DISPLAY -----
